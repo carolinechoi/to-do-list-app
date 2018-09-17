ee66542503eb69222217c211c66b5ee265a210de
Caroline <38522055+carolinechoi@users.noreply.github.com>
2018-09-16 23:34:15 -0700

    added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireModule } from '@angular/fire';
+import { FIREBASE_CREDENTIALS } from './firebase.credentials';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -18,6 +22,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CREDENTIALS)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CREDENTIALS = { 
+    apiKey: "AIzaSyBm4WNlF2uG0zDpchAEnW-mRe31HVdUOfQ",
+    authDomain: "to-do-list-app-b9a9b.firebaseapp.com",
+    databaseURL: "https://to-do-list-app-b9a9b.firebaseio.com",
+    projectId: "to-do-list-app-b9a9b",
+    storageBucket: "to-do-list-app-b9a9b.appspot.com",
+    messagingSenderId: "1040001891586"
+}
