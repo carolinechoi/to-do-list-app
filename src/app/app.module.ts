93762f10bc555160d5be05da2a104d425b5d9130
Caroline <38522055+carolinechoi@users.noreply.github.com>
2018-09-16 23:56:41 -0700

    added signup using afauth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,32 @@
+//Default imports
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+//Firebase imports
 import { AngularFireModule } from '@angular/fire';
 import { FIREBASE_CREDENTIALS } from './firebase.credentials';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+
+//Page imports
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+import { LandingPage } from '../pages/landing/landing';
+import { LoginPage } from '../pages/login/login';
+import { SignupPage } from '../pages/signup/signup';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    LandingPage,
+    LoginPage,
+    SignupPage
   ],
   imports: [
     BrowserModule,
@@ -29,7 +38,10 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    LandingPage,
+    LoginPage,
+    SignupPage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/signup/signup.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { IonicPage, ToastController, NavController, NavParams } from 'ionic-angular';
+
+//Firebase imports
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+
+//Model import
+import { User } from '../../models/user.interface';
+
+//Pages import
+import { HomePage } from '../home/home';
+
+@IonicPage()
+@Component({
+  selector: 'page-signup',
+  templateUrl: 'signup.html',
+})
+export class SignupPage implements OnInit{
+  
+  newUser = {} as User;
+  newUserRef$: AngularFireList<User>;
+
+  constructor(private toast: ToastController, 
+    private db: AngularFireDatabase, 
+    private afauth: AngularFireAuth, 
+    public navCtrl: NavController, 
+    public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SignupPage');
+  }
+
+  ngOnInit() {
+    this.newUserRef$ = this.db.list('users');
+  }
+
+  async addUser(newUser: User) {
+    try {
+    const result = await this.afauth.auth.createUserWithEmailAndPassword(newUser.email, newUser.password);
+    console.log(result);
+    if (result) {
+      this.navCtrl.push(HomePage);
+    }
+    }
+    catch(e) {
+      this.toast.create({
+        message: e,
+        duration: 2000
+      }).present();
+    }
+  }
+
+}
